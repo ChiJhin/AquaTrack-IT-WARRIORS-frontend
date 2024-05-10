d89439afc061c465219ba98495e81ac9bd09a7cc
Valeriia295 <sokolsokol295@gmail.com>
2024-05-10 23:22:41 +0200

    sign-in-and-sign-up-forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
-// import { useDispatch } from "react-redux";
-import { lazy } from 'react';
-// import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoute.jsx';
-// import { PrivateRoute } from './components/PrivateRoute/PrivateRoute.jsx';
-// import { refreshUser } from "./redux/auth/operations.js";
-// import { useAuth } from "./hooks";
-
-const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
-const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage.jsx'));
-const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
-const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
+import HomePage from './pages/HomePage/HomePage.jsx';
+import SignInPage from './pages/SignInPage/SignInPage.jsx';
+import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
+import TrackerPage from './pages/TrackerPage/TrackerPage.jsx';
 
 export default function App() {
   return (

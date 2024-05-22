4146635db3cabeaefc5e04a7d5ee3ee22eae722c
ChiJhin <jerek.d.lem@gmail.com>
2024-05-22 04:50:05 +0000

    Deploying to gh-pages from @ ChiJhin/AquaTrack-IT-WARRIORS-frontend@12aa45c88c5ea0ffc9dd066b8778fab7c2bd52ed 🚀

---
+++ assets/Logo-ccd42efe.js
@@ -0,0 +1,1 @@
+import{j as o,L as n}from"./index-39a19b5e.js";const t="_logoFont_12yj7_1",s="_logo_12yj7_1",l={logoFont:t,logo:s};function r(){return o.jsx(o.Fragment,{children:o.jsx(n,{to:"/",className:l.logoFont,children:"AQUATRACK"})})}export{r as L};
